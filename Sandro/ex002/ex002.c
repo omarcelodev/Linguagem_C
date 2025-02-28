6dbc79d7f5971e0ff235a36ca25f9782da33af89
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-02-28 20:43:13 -0300

    Correções

--- Sandro/ex002/ex002.c
+++ Sandro/ex002/ex002.c
@@ -1,21 +1,25 @@
-/*Ex002: Escreva um prg em C que leia o raio de um circulo e calcule
+/*
+Autor:Marcelo
+Data: 25/02/2025
+Função do programa: Calular a Area da circunferencia e apresenta-la ao usuario
+
+Ex002: Escreva um prg em C que leia o raio de um circulo e calcule
 e imprima a sua area. Crie uma constante utilizando o qualificador
 const da linguagem C para armazenar o valor de pi.*/
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
     float raio=0, area=0;
-    const float PI=3.14;
 
     printf("Digite o raio da circunferencia: ");
     scanf("%f", &raio);
 
-    area = (PI * raio * raio);
+    area = M_PI * pow(raio, 2);
 
     printf("O valor da area dessa circunferencia e: %.2f", area);
 
-
-
-    return 0;}
+    return 0;
+}
