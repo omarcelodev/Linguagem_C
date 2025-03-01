8f47ba92939bf96ebe5078b81109b1a7b4ab16e6
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-03-01 10:35:39 -0300

    desafio

--- Desafios/Sorteio/sorteio.c
+++ Desafios/Sorteio/sorteio.c
@@ -3,19 +3,25 @@
 #include <time.h>
 
 int main(){
-    int num1, num2, num3;
+    int min, max, num3;
 
-    printf("Voce vai digitar dois numeros inteiros, um definindo o limite minimo\n e o outro o limite maximo.\n");
+    printf("Voce vai digitar dois numeros inteiros, um definindo o limite minimo\ne o outro o limite maximo.\n");
 
     printf("Digite o primeiro numero: ");
-    scanf("%d", &num1);
+    scanf("%d", &min);
 
     printf("Digite o segundo numero: ");
-    scanf("%d", &num2);
+    scanf("%d", &max);
 
     srand(time(NULL));
 
-    num3 = rand()%num2 + num1;
+    if(min > max){
+        int temp = min;
+        min = max;
+        max = temp;
+    }
+
+    num3 = rand()%(max - min + 1) + min;
 
     printf("O numero sorteado e: %d", num3);
 
